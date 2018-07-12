982d12e4dbfdcbb83258253fb1f6324d6b114b8d
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-12 09:19:10 -0600

    One hour deadline

---
+++ epic/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home Page</title>
+		<link rel="stylesheet" href="./style.css">
+	</head>
+	<body>
+		<h1 class="center">Persona</h1><br/><br/>
+		<img class="center" src="images/rocker.jpg">
+		<div>
+			<ul>
+				<li>Name: Dave</li>
+				<li>Gender: Male</li>
+				<li>Age: 21</li>
+				<li>Marital status: Single</li>
+				<li>Profession: Student/Part-time Retail</li>
+				<li>Annual Income: $25k</li>
+			</ul>
+		</div>
+		<div>
+			<ul>Attitude</ul>
+			<ul>Music is about the holistic experience: good acoustics, good beer, good seats/standing room, and even good parking.  For him to be able to enjoy a concert to the fullest, he doesn’t want to have to stand in line for hours to pick up his tickets, or to have to elbow his way through a crowd in a dimly-lit room to buy a drink.  He wants a way to let venues and other concert-goers know what features he thinks are critical to making a great concert experience.</ul>
+		</div>
+		<div class="container-right">
+			<ul>Technological Needs (in order of preference):
+				<li>Ability to review local (city) concert venues</li>
+				<li>Ability to share favorite venues with friends</li>
+			</ul>
+		</div>
+		<div>
+			<ul>Frustrations:
+				<li>Services like Yelp provide reviews for venues, but the venues are not always used for concerts, and are therefore not always relevant</li>
+				<li>No specific site exists where people like him can gather to share favorites</li>
+			</ul>
+		</div>
+		<div>
+			<ul>Technology (comfort level - high):</ul>
+			<ul>Mostly mobile tech, and only uses his laptop for school or Netflix or shopping online.  He has an older version of the iPhone due to his intermittent income, as a student, but he does keep his iOS updated so he can keep his plethora of apps updated.
+				<li>iPhone 8, on iOS 11.x</li>
+				<li>Dell - Inspiron 15.6" Laptop - AMD A6-Series - 4GB Memory - AMD Radeon R4 - 500GB Hard Drive</li>
+				<li>Xbox One and Samsung Smart TV</li>
+			</ul>
+		</div>
+	</body>
+</html>
+
+
+
+
+<?php
+/**
+
+ */
